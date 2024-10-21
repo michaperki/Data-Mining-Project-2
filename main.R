a3e48f0ac0b122486e9fb45950a3c0c620fab273
oahofmann <ohofmann@smu.edu>
2024-10-21 15:32:26 -0500

    Added Responsibilities

--- main.R
+++ main.R
@@ -3,3 +3,8 @@ library("ggplot2")
 library("DT")
 
 cases <- read_csv("data/COVID-19_cases_plus_census.csv")
+
+# Olivia - Income/Wealth
+# Matias - Population Density
+# Mike - Exceptional Credit Clustering
+
